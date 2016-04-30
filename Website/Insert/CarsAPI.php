3f615ca71fdde83bb1bb6b6577aba645566e803a
Jlspncer <j-spencer.4@onu.edu>
2016-04-29 21:16:11 -0400

    Added files via upload

---
+++ Website/Insert/CarsAPI.php
@@ -0,0 +1,90 @@
+<?php
+
+require("Credentials.php");
+
+echo $_POST["method"]();
+
+function sanitize($str, $quotes = ENT_NOQUOTES){
+    $str = htmlspecialchars($str, $quotes);
+    return $str;
+}
+
+function getCars(){
+    $dbConn = mysqli_connect(server(), username(), password(), db());
+
+    $query = "SELECT * FROM LuxuryCarModels";
+    $result = $dbConn->query($query);
+    if ($dbConn->connect_error) {
+        $return->connect_error = "Connection failed: " . $dbConn->connect_error;
+        $return->success = false;
+        return json_encode($return);
+    }
+
+    $cars = array();
+
+    if($result) {
+        while($row = $result->fetch_array()){
+            $allColunms = array();
+            for ($i = 0; $i < 7; $i++){
+                array_push($allColumns, $row[$i]);
+            }
+            array_push($cars, $allColumns);
+        }
+    }
+
+    $return = new StdClass();
+    $return->success = true;
+    $return->cars = $cars;
+    $return->querystring = $query;
+    return json_encode($return);
+}
+
+function insertCars(){
+    if(isset($_POST['ID'])){
+        $ID = json_decode(sanitize($_POST['ID']));
+    }
+
+    if(isset($_POST['Model'])){
+        $Model = json_decode(sanitize($_POST['Model']));
+    }
+
+    if(isset($_POST['Engine'])){
+        $Engine = json_decode(sanitize($_POST['Engine']));
+    }
+
+    if(isset($_POST['MPG'])){
+        $MPG = json_decode(sanitize($_POST['MPG']));
+    }
+
+    if(isset($_POST['Price'])){
+        $Price = json_decode(sanitize($_POST['Price']));
+    }
+
+    if(isset($_POST['Drivetrain'])){
+        $Drivetrain = json_decode(sanitize($_POST['Drivetrain']));
+    }
+
+    $dbConn = mysqli_connect(server(), username(), password(), db("Cars"));
+
+    if($dbConn->connect_error){
+        die("Connection failed: " . $dbConn->connect_error);
+    }
+
+    $query = "INSERT INTO LuxuryCarModels (ID, Model, Engine, MPG, Price, Drivetrain ) " .
+        "VALUES ( " .
+        "" . $ID . ", " .
+        "" . $Model . ", " .
+        "'" . $Engine . "', " .
+        "'" . $MPG . "', " .
+        "'" . $Price . "', " .
+        "'" . $Drivetrain . "' );";
+    $result = $dbConn->query($query);
+    $return = new stdClass;
+    $return->querystring = (string) $query;
+    if ($result) {
+        $return->success = true;
+    } else {
+        $return->success = false;
+    }
+    return json_encode($return);
+}
---
+++ Website/Insert/LuxuryCars.js
@@ -0,0 +1,92 @@
+var cars;
+
+function onLoad(){
+    getCars(false);
+}
+
+function insertCars() {
+    alert("In insertCarsCall()")
+var ID, Model, Engine, MPG, Price, Drivetrain;
+    ID = JSON.stringify($("#ID").val());
+    Model = JSON.stringify($('#Model').val());
+    Engine = JSON.stringify($('#Engine').val());
+    MPG = JSON.stringify($('#MPG').val());
+    Price = JSON.stringify($('#Price').val());
+    Drivetrain = JSON.stringify($('#Drivetrain').val());
+    ajax = ajaxInsertCar("insertCar", Model, Engine, MPG, Price, Drivetrain);
+    ajax.done(insertCarCallback);
+    ajax.fail(function () {
+        alert("Failure in insertCar");
+    });
+}
+
+function ajaxInsertCars (method, ID, Model, Engine, MPG, Price, Drivetrain){
+    return $.ajax({
+        url: 'CarsAPI.php',
+        type: 'POST',
+        data: {method: method,
+            ID: ID,
+            Model: Model,
+            Engine: Engine,
+            MPG: MPG,
+            Price: Price,
+            Drivetrain: Drivetrain}
+    });
+}
+
+function insertCarsCallback(response_in){
+    alert("In insertCarsCallback()")
+    response = JSON.parse(response_in);
+
+    if(!response['success']){
+       $("#results").html("");
+       alert("Insert failed.");
+    } else {
+        $("results").html(
+            response['querystring'] + '<br>' +
+            response['success'] + '<br>');
+        getCars();
+    }
+}
+
+function showCars(cars){
+    alert("In showCars()");
+    alert(Cars);
+    var carList = "";
+        $.each(cars, function (key, value){
+            var itemStr = "";
+            $.each(value, function(key, item){
+                itemStr += item + '&nbsp' + '&nbsp';
+            });
+            carList += itemStr + '<br>';
+        });
+        $("#cars").html(carList);
+}
+
+function getCars(){
+    ajax = ajaxGetCars("getCars");
+    ajax.done(getCarsCallback);
+    ajax.fail(function () {
+        alert("Failure in getCars call to ajaxGetCars");
+    });
+}
+
+function ajaxGetCars(method){
+    alert("In ajaxGetCars()");
+    return $.ajax({
+        url: 'CarsAPI.php',
+        type: 'POST',
+        data:{method: method}
+        });
+    }
+
+function getCarsCallback(response_in){
+    alert(response_in);
+    var response = JSON.parse(response_in);
+    cars = response["cars"];
+    if(!response['success'])
+       $("#results").html("getCars() failed")
+    else {
+        showCars(cars);
+    }
+}
